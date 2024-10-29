ed4d870c2d9feb8e07c54a6dc8f25cdb19d95bf2
Erick Clinton <erickleite2009321@gmail.com>
2024-10-29 11:45:48 -0300

    dasdas

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "hello_world" {
   filename          = data.archive_file.lambda_zip.output_path  # Aponta para o arquivo zip gerado
   source_code_hash  = data.archive_file.lambda_zip.output_base64sha256  # Usa o hash gerado pelo recurso
 
-  role = "arn:aws:iam::767397741290:user/teste"
+  role = "arn:aws:iam::767397741290:role/teste"
 }
 
 
